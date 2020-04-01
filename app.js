74cfbe3eaff470ddeae3501beee9418f343e56bc
Simas <simas@simas-dei.com>
2020-04-01 19:17:00 +0300

    add connection to mongodb

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const database = require('./util/database');
 const errorController = require('./controllers/error');
 
 const app = express();
@@ -10,15 +11,18 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {});
-
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+database(client => {
+	app.listen(3000);
+	console.log(client);
+});
--- util/database.js
+++ util/database.js
@@ -1,8 +1,21 @@
-const Sequelize = require('sequelize');
+const { MongoClient } = require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const formatedString = () => {
+	const db = process.env.MONGO_URL.replace('<USER>', process.env.MONGO_USER).replace(
+		'<PASSWORD>',
+		process.env.MONGO_PASSWORD
+	);
+	return db;
+};
 
-module.exports = sequelize;
+const database = async callback => {
+	try {
+		const connection = await MongoClient.connect(formatedString());
+		console.log('Connected to database 💥🎉');
+		callback(connection);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+module.exports = database;
